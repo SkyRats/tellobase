8acdb7c5a84a5504440e9cf4f119fa993c4fdf0e
Bárbara_Bueno <65515809+Buenobarbie@users.noreply.github.com>
2023-02-09 10:38:00 -0300

    Adicionando o loop do jogo

--- scripts/ttt.py
+++ scripts/ttt.py
@@ -1,6 +1,24 @@
 import cv2
 import numpy as np
 
+'''
+Class tttDetection
+--------------------------------------------------------------------------------------
+__init__()            # inicia a captura de tela e o contorno dos players
+filter_small()        # 
+match_shape ()        # 
+most_frequent()       # 
+get_common()          # 
+get_mask()            # Gera a máscara de cores no range informado
+get_squares()         # Identifica uma região com área mínima na cor desejada (azul)
+detect_board()        # Identifica o tabuleiro na imagem e retorna suas dimensões e coordenadas
+read_board()          # Lê o tabuleiro e retorna seu estado atual de jogadas
+print_board()         # Imprime o estado do tabuleiro no terminal
+play_ttt()            # Conduz o jogo, esperando e lendo as jogadas
+--------------------------------------------------------------------------------------
+
+
+'''
 
 class tttDetection:
     
@@ -8,6 +26,7 @@ class tttDetection:
         self.capture = cv2.VideoCapture(0)
         #self.frame = cv2.imread("./ttt1.jpeg")
 
+        # Contornos do player1 (X)
         player1 = cv2.imread("player1.png")
         player1_gray = 255 - cv2.cvtColor(player1, cv2.COLOR_BGR2GRAY)
         
@@ -15,6 +34,7 @@ class tttDetection:
         contours, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         self.player1_cnt = contours[0]
 
+        # Contornos do player2 (Check mark)
         player2 = cv2.imread("player2.png")
         player2_gray = 255 - cv2.cvtColor(player2, cv2.COLOR_BGR2GRAY)
 
@@ -36,15 +56,15 @@ class tttDetection:
         for contour in square_cnts:
                         
             if cv2.matchShapes(contour, self.player1_cnt, 1, 0.0) < max_tolerance:
-                print("player1")
-                return "player1"
+                # print("player1")
+                return "x"
 
             elif cv2.matchShapes(contour, self.player2_cnt, 1, 0.0) < max_tolerance:
-                print("player2")
-                return "player2"
+                # print("player2")
+                return "v"
 
-        print("not played")
-        return "not played"
+        # print("not played")
+        return " "
     
     def most_frequent(self, list):
 
@@ -79,28 +99,6 @@ class tttDetection:
         mask = cv2.inRange(hsv , lower, upper)
         return mask
 
-    def get_square_area(self,img):
-        # Retorna uma lista com os quadrados que tem area maior ou igual a min_area
-        square_detected = 0
-
-        min_area = 6000 # Area minima para um quadrado ser contabilizado
-    
-        contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # Se quiser ver os contornos:
-        #cv2.drawContours(self.frame,contours,-1,(255,0,0),3)
-        
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area >= min_area:
-
-                x,y,w,h=cv2.boundingRect(contour)
-                square_detected = [x,y,w,h]
-
-                # Desenha um retangulo em torno do quadrado detectado (opcional)
-                #cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),3)
-
-        return square_detected
-
     def get_squares (self, image):
         
         # Mascara azul
@@ -125,87 +123,144 @@ class tttDetection:
         blue_result = cv2.dilate(blue_result, dilate_kernel)
         blue_result = cv2.erode(blue_result, erode_kernel)
 
-        # Retorna a contagem de quadrados verdes e vermelhos e as imagens para visualizacao das mascaras
-        return self.get_square_area(blue_mask), blue_result
+        # Retorna uma lista com os quadrados que tem area maior ou igual a min_area
+        square_detected = 0
+
+        min_area = 6000 # Area minima para um quadrado ser contabilizado
     
-    def detect_board(self, tries = 1, max_tolerance = .6, wait_time = 50):
-        boards = []
+        contours, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Se quiser ver os contornos:
+        #cv2.drawContours(self.frame,contours,-1,(255,0,0),3)
+        
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area >= min_area:
+
+                x,y,w,h=cv2.boundingRect(contour)
+                square_detected = [x,y,w,h]
+
+                # Desenha um retangulo em torno do quadrado detectado (opcional)
+                #cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),3)
+
+        return square_detected, blue_result
+    
+    def detect_board(self, tries = 1):
+        # ?? Tirar o tries já que ele vai rodar enquanto não achar o tabuleiro?
         for _ in range(tries):
             success, self.frame = self.capture.read(0)
             if success == False:
                 raise ConnectionError
+            self.frame = cv2.imread("./ttt1.jpeg")
+            board, self.blue_image = self.get_squares(self.frame)
+            return board
+    
+    def read_board(self, board, max_tolerance = .6, wait_time = 50):
+        self.boards = []
+        x,y,w,h = board
 
-            board, blue_image = self.get_squares(self.frame)
+        """
+        board_squaresX = [ 
+            [x + w * (1/6), x + w * (3/6), x + w * (5/6)],
+            [x + w * (1/6), x + w * (3/6), x + w * (5/6)],
+            [x + w * (1/6), x + w * (3/6), x + w * (5/6)]
+        ]
 
-            if board != 0:
+        board_squaresY = [ 
+            [y + h * (3/6), y + h * (3/6), y + h * (3/6)],
+            [y + h * (5/6), y + h * (5/6), y + h * (5/6)],
+            [y + h * (1/6), y + h * (1/6), y + h * (1/6)]
+        ]
+        """
+        
+        board_simple = [[], [], []]
+        frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        _, frame_thresh = cv2.threshold(frame_gray, 127, 255,0)
 
-                # Para ver as imagens das mascaras aplicadas e da camera usada
-                x,y,w,h = board
+        cv2.imshow("frame threshold", frame_thresh)
 
-                """
-                board_squaresX = [ 
-                    [x + w * (1/6), x + w * (3/6), x + w * (5/6)],
-                    [x + w * (1/6), x + w * (3/6), x + w * (5/6)],
-                    [x + w * (1/6), x + w * (3/6), x + w * (5/6)]
-                ]
-
-                board_squaresY = [ 
-                    [y + h * (3/6), y + h * (3/6), y + h * (3/6)],
-                    [y + h * (5/6), y + h * (5/6), y + h * (5/6)],
-                    [y + h * (1/6), y + h * (1/6), y + h * (1/6)]
-                ]
-                """
+        for i in range(3):
+            for j in range(3):
                 
-                board_simple = [[], [], []]
-                frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-                _, frame_thresh = cv2.threshold(frame_gray, 127, 255,0)
+                square = frame_thresh[(y + h * (j)//3) : (y + h * (j + 1)//3), (x + w * (i)//3) : (x + w * (i + 1)//3)]
+                square_cnts, _ = cv2.findContours(square, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                square_cnts = self.filter_small(square_cnts, 25)
 
-                cv2.imshow("frame threshold", frame_thresh)
+                # print("row: ", j + 1, "collum: ", i + 1)
 
-                for i in range(3):
-                    for j in range(3):
-                        
-                        square = frame_thresh[(y + h * (j)//3) : (y + h * (j + 1)//3), (x + w * (i)//3) : (x + w * (i + 1)//3)]
-                        square_cnts, _ = cv2.findContours(square, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                        square_cnts = self.filter_small(square_cnts, 25)
+                board_simple[j].append(self.match_shape(square_cnts, max_tolerance))
 
-                        print("row: ", j + 1, "collum: ", i + 1)
+                # print("------------------")
+                # square = cv2.cvtColor(square, cv2.COLOR_GRAY2BGR)
+                # cv2.imshow("", square)
 
-                        board_simple[i].append(self.match_shape(square_cnts, max_tolerance))
-
-                        print("------------------")
-                        square = cv2.cvtColor(square, cv2.COLOR_GRAY2BGR)
-                        cv2.imshow("", square)
-
-                cv2.waitKey(wait_time)
-                cv2.imshow('blue', blue_image)
-                print("#----------------#")
-                boards.append(board_simple)
-            
-        board_simple = self.get_common(boards)
-        print("result after", tries, "tries:")
+        cv2.waitKey(wait_time)
+        cv2.imshow('blhttps://github.com/SkyRats/tellobase.gitue', self.blue_image)
+        print("#----------------#")
+        self.boards.append(board_simple)
+        
+        board_simple = self.get_common(self.boards)
+        # print("result after", tries, "tries:")
         print(board_simple)
         return board_simple
 
-# Enquanto não chegar ao fim do tubo, ir lendo os sensores da tela
-if __name__ == "__main__":
-    
-    #adjust the board
-    capture = cv2.VideoCapture(0)
-    while(True):
+    def print_board(self, board):
+        print(f" {board[0][0]} | {board[0][1]} | {board[0][2]}")
+        print("-----------")
+        print(f" {board[1][0]} | {board[1][1]} | {board[1][2]}")
+        print("-----------")
+        print(f" {board[2][0]} | {board[2][1]} | {board[2][2]}")
+        print()
+        print()
+        print("###########################################")
 
+    def play_ttt(self):
+        previous_board = []
+        board_state = []
+        start_detetection = False
+        board = 0
+
+        print("Começando o jogo da velha!!")
+        print("-------------------------------")
+
+        # Enquanto o jogo não tiver terminado
+        while True:
+            start_detetection = input("Digite qualquer coisa para iniciar uma detecção  ")
+
+            if start_detetection:
+
+                # Detectando o tabuleiro
+                while board == 0:
+                    print("Buscando o tabuleiro na imagem...")
+                    board = self.detect_board()
+                print("Tabuleiro encontrado!\n")
+                
+
+                # Lendo o estado do jogo
+                board_state = self.read_board(board)
+                print("\nSituaçaõ atual do jogo:")
+                self.print_board(board_state)
+
+            
+            start_detetection = False
+                
+
+
+            
+
+if __name__ == "__main__":
+    #adjust the board
+    # capture = cv2.VideoCapture(0)
+    # while(True): 
+    #     success, frame = capture.read(0)
+    #     if cv2.waitKey(1) == ord(" "):
+    #         break
+    #     cv2.imshow("line up board", frame)
         
-        success, frame = capture.read(0)
-        if cv2.waitKey(1) == ord(" "):
-            break
-        cv2.imshow("line up board", frame)
-        
-    capture.release()
+    # capture.release()
 
     detecting = tttDetection()
-    while True:
-        board = detecting.detect_board(25)
-        cv2.waitKey(0)
+    detecting.play_ttt()
+    cv2.waitKey(0)
 
     detecting.capture.release()
     cv2.destroyAllWindows()
